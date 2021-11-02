755e35663dee86d669bd5b2178490947ce52e62e
Mike Barkas <mikebarkas1@gmail.com>
2021-11-02 16:00:42 -0400

    Initial service test

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 var build = "develop"
 
 func main() {
+	log.Println("Service starting", build)
+	defer log.Println("Service stopped")
 
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdown
+
+	log.Println("stopping service")
 }
