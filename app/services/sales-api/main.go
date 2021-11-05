682609f2175436465dfa7aeddb56f29ddfda09b0
Mike Barkas <mikebarkas1@gmail.com>
2021-11-05 08:59:45 -0400

    Implement Sugar logger in main and start run func

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -2,31 +2,50 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
-	"os/signal"
-	"runtime"
-	"syscall"
 
-	"go.uber.org/automaxprocs/maxprocs"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var build = "develop"
 
 func main() {
 
-	if _, err := maxprocs.Set(); err != nil {
-		fmt.Println("maxprocs: %w", err)
+	// Create the app logger.
+	log, err := initLogger("SALES-API")
+	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
-	g := runtime.GOMAXPROCS(0)
+	defer log.Sync()
 
-	log.Printf("Starting service build[%s] CPI[%d]", build, g)
-	defer log.Println("Service stopped")
+	// Perform startup and shutdown.
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		os.Exit(1)
+	}
+}
 
-	shutdown := make(chan os.Signal, 1)
-	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
-	<-shutdown
+func run(log *zap.SugaredLogger) error {
+	return nil
+}
 
-	log.Println("stopping service")
+// initLogger constructs a Sugared Logger that writes to stdout and
+// provides human readable timestamps.
+func initLogger(service string) (*zap.SugaredLogger, error) {
+	config := zap.NewProductionConfig()
+	config.OutputPaths = []string{"stdout"}
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.DisableStacktrace = true
+	config.InitialFields = map[string]interface{}{
+		"service": service,
+	}
+
+	log, err := config.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	return log.Sugar(), nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mikebarkas/service3
 
 go 1.16
 
-require go.uber.org/automaxprocs v1.4.0
+require (
+	go.uber.org/automaxprocs v1.4.0
+	go.uber.org/zap v1.19.1
+)
